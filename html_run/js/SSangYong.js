003c9e4e11e3e74b2029796c12db0e9764c522c2
greenwhale00 <105770998+greenwhale00@users.noreply.github.com>
2022-06-28 17:48:48 +0900

    Add files via upload

--- html_run/js/SSangYong.js
+++ html_run/js/SSangYong.js
@@ -1,10 +1,58 @@
+$(function(){
+
+
 $('.slider').slick({
     arrows: false,
-    autoplay: false,
-    dots: true,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    autoplaySpeed: 4000,
-    centerMode: true,
-    slidesToShow: 5,
+    dots: false,
 });
+
+$('.productArrows i:nth-child(1)').on('click', function () {
+    $('.slider').slick('slickPrev')
+});
+$('.productArrows i:nth-child(2)').on('click', function () {
+    $('.slider').slick('slickNext')
+});
+
+
+
+});
+
+
+
+
+
+//#slick {width: 1200px; height: 400px;margin: 0 auto;overflow: hidden;}
+
+    
+    
+   
+
+
+//#slick .slider .inbox figure {
+   // padding: 0 300px;
+//}
+
+//#slick .slider .inbox {
+ //   margin: 0 0 100px 0;
+//}
+
+//#slick .slider .slick-dots {
+   // padding: 20px;
+//}
+
+//#slick .slider .slick-dots li {
+  //  display: inline-block;
+    //margin: 0 2px;
+//}
+
+//#slick .slider .slick-dots li button {
+ //   border: none;
+//    outline: none;
+ //   padding: 0 0;
+   // background: #f00;
+  //  color: #fff;
+    //border-radius: 50%;
+   // height: 20px;
+   // width: 20px;
+    //font-size: 13px;
+//}
