fca44f02814e2749d0dc02b305fee336f6f135fe
greenwhale00 <105770998+greenwhale00@users.noreply.github.com>
2022-06-28 12:26:25 +0900

    Add files via upload

--- html_run/js/SSangYong.js
+++ html_run/js/SSangYong.js
@@ -1,1 +1,10 @@
-$('.slider').slick();
+$('.slider').slick({
+    arrows: false,
+    autoplay: false,
+    dots: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    autoplaySpeed: 4000,
+    centerMode: true,
+    slidesToShow: 5,
+});
