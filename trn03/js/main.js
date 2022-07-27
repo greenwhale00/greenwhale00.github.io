ae66d4c4de934ee8adc0c4fcd7899967f2043c3b
greenwhale00 <105770998+greenwhale00@users.noreply.github.com>
2022-07-27 17:25:50 +0900

    Add files via upload

---
+++ trn03/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    $('.TopBanner i').on('click', function () {
+        $('.TopBanner').hide();
+    })
+
+    $('.pop button').on('click', function () {
+        $(this).parent().hide();
+
+    })
+
+})
+
+
+
+
+
+
